b6edcc4e922d7895983f70dab266280ce95ad50c
StanislasE <stanislas.etienne@gmail.com>
2020-11-18 15:33:46 +0100

    fait

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 6</title>
+</head>
+<body>
+    
+<?php
+    $number = 140;
+    echo $number . ' - ';
+    $number = ($number + 30);
+    echo $number . ' - ' ;
+    $number = ($number / 2);
+    echo $number ;
+?>
+
+</body>
+</html>
